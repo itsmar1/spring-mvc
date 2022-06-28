91904efe8a8ddf03b60c61f2e8342595eb17daa0
Marouane Ahal <d144013152@gmail.com>
2022-06-28 13:18:31 +0100

    reading HTML form data P:2

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloWorldController {
 
     // need a controller method to show the form
-    @RequestMapping("showForm")
+    @RequestMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
 
     // need a controller method to process the HTML form
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
 }
---
+++ web/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dell
+  Date: 6/28/2022
+  Time: 1:12 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello World - process form</title>
+</head>
+<body>
+
+Hello World of Spring!
+<br><br>
+
+Student name: ${param.studentName}
+
+</body>
+</html>
